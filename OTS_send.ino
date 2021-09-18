0b1c49f82c28ae1661fdabcb8d9d4a6df4156fcd
peter <peter9167@naver.com>
2021-09-18 23:02:09 +0900

    first commit

---
+++ OTS_send.ino
@@ -0,0 +1,18 @@
+#include <DHT.h>
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+  
+
+void setup(){
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop(){
+  delay(1000);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();  
+  
+  Serial.println(String(t) + "," + String(h) +",");
+}
